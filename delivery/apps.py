5839691eb43dc2a22770522beaec34d13b360fdb
Blessian <blessian.dev@gmail.com>
2022-09-12 18:27:13 +0900

    initial commit

--- delivery/apps.py
+++ delivery/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GlobaldeliveryConfig(AppConfig):
+class DeliveryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'globaldelivery'
+    name = 'delivery'
